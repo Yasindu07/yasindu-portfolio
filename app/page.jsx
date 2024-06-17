fa3cdf4989b6f0692f72d0b6f961cf83f1d49c0b
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-06-18 01:43:25 +0530

    cv

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Photo from "@/components/Photo";
 import Socials from "@/components/Socials";
 import Stats from "@/components/Stats";
+import Link from "next/link";
 import { Button } from "@/components/ui/Button";
 import { FiDownload } from "react-icons/fi";
 
@@ -23,15 +24,22 @@ const Home = () => {
             </p>
             {/* button and social */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex
-                items-center gap-2"
-              >
+              <Link
+                  href="/assets/resume/YasinduCV.pdf"
+                  target="_blank"
+                  alt=""
+                  rel="noopener noreferrer"
+                >
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex
+                  items-center gap-2"
+                >
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
-              </Button>
+                </Button>
+              </Link>
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
