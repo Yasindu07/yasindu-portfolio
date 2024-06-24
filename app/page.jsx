ba0d9ccae91e46c9e03fe7cc920c21920d1eb45b
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-06-24 15:42:07 +0530

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -24,7 +24,7 @@ const Home = () => {
             {/* button and social */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <a
-                  href="https://firebasestorage.googleapis.com/v0/b/yasindu-portfolio.appspot.com/o/YasinduCV.pdf?alt=media&token=fd758b33-3621-472c-ba42-94c9b2ade583"
+                  href="https://firebasestorage.googleapis.com/v0/b/yasindu-portfolio.appspot.com/o/YasinduCV.pdf?alt=media&token=72bf3984-4094-481c-9857-e328b4708b5d"
                   target="_blank"
                   download="YasinduCV.pdf"
                 >
